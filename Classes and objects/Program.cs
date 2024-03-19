72be01c747840da7eb1aff2a202d9e77f5a1f239
Максим Кутаев <maximkut.01@gmail.com>
2024-03-19 23:17:04 +0200

    Добавьте файлы проекта.

---
+++ Classes and objects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Classes_and_objects;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Worker worker1 = new Worker("Sergiy", "Rebrov", 1230.30, 16);
+
+        Console.WriteLine("Дані JSON файлу:");
+
+        Worker.SaveToJson(worker1, "worker1.json");
+
+        Worker workerFromFile = Worker.LoadFromJson("worker1.json");
+
+        Console.WriteLine($"Працівник: {workerFromFile.Firstname} {workerFromFile.Lastname}\nЗаробітна плата: {workerFromFile.Pay * workerFromFile.Days}");
+        Console.ReadKey();
+    }
+}
+
---
+++ Classes and objects/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Classes_and_objects
+{
+    class Worker
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public double Pay { get; set; }
+        public int Days { get; set; }
+        public Worker() { }
+
+        public Worker(string _Firstname, string _Lastname, double _Pay, int _Days) 
+        {
+            Firstname = _Firstname;
+            Lastname = _Lastname;
+            Pay = _Pay;
+            Days = _Days;
+            Console.WriteLine($"Дані про працівника: \n Ім'я: {Firstname} \n Прізвище: {Lastname} \n Добова ставка: {Pay} \n Відпрацьовані дні: {Days}");
+            salary();
+        }
+        
+        public void salary()
+        {
+            double salary = Pay * Days;
+            Console.WriteLine("Заробітна плата складає: " + salary);
+        }
+
+        public static void SaveToJson(Worker worker, string filePath)
+        {
+            var options = new JsonSerializerOptions();
+            string jsonString = JsonSerializer.Serialize(worker, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public static Worker LoadFromJson(string filePath)
+        {
+            string jsonString = File.ReadAllText(filePath);
+            Worker worker = JsonSerializer.Deserialize<Worker>(jsonString);
+            return worker;
+        }
+    }
+}
